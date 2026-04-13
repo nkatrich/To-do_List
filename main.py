272a5c39b489fad21bb382d3aa57005d4a207e51
Nikita <nikitakatrich.kzn@gmail.com>
2026-04-13 20:45:17 +0300

    made logic in func of getting data when needed, CLI greeting with notes(minimal ui)

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ with open(file_name, "w") as file:
     json.dump(my_stats, file, indent=4)
     print("Data saved to data.json!")
 
-if os.path.exists(file_name):
-    with open(file_name, "r") as file:
-        loaded_data = json.load(file)
-        print(f"Biography: Name is {loaded_data["Programmer"]}, organisation is {loaded_data["Channel"]}.")
+def get_data():
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            return json.load(file)
+        
+data = get_data()
+
+print('TO-DO List ready for notes!')
+print(f'''Current notes:
+{data}''')
