69de36be4596f31896a2bcad1703dd3f2cb33070
Nikita <nikitakatrich.kzn@gmail.com>
2026-04-16 18:17:45 +0300

    program save data even after rerun program

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ import json
 import os
 
 file_name = "data.json"
-stats = []
 
 def in_data():
     with open(file_name, "w") as file:
-        json.dump(stats, file, indent=4)
+        json.dump(data, file, indent=4)
         print("Data saved to data.json!")
 
 def get_data():
     if os.path.exists(file_name):
         with open(file_name, "r") as file:
             return json.load(file)
+    else:
+        return {}
 
-def show_notes():
-    data = get_data()
-    print(f'''Current notes:
-{data}''')
+def show_notes(data):
+    print('Current notes:')
+    [print(i) for i in data["Notes"]]
 
+data = get_data()
 print('TO-DO List ready for notes!\n')
 while True:
     note = input("Type note or key word for exit: EXIT\n")
     if note == 'EXIT':
         print('\nProgramm finished. All data saved.')
         break
-    stats.append(note)
+    data["Notes"].append(note)
+    in_data()
+    show_notes(data)
