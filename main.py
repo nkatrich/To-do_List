3e24041614c35fb104e643a1d0db231e60d55c8c
Nikita <nikitakatrich.kzn@gmail.com>
2026-04-16 18:34:55 +0300

    made check if data present or absence

--- main.py
+++ main.py
@@ -6,26 +6,24 @@ file_name = "data.json"
 def in_data():
     with open(file_name, "w") as file:
         json.dump(data, file, indent=4)
-        print("Data saved to data.json!")
+        print("Data have been saved!\n")
 
 def get_data():
     if os.path.exists(file_name):
         with open(file_name, "r") as file:
             return json.load(file)
-    else:
-        return {}
 
 def show_notes(data):
-    print('Current notes:')
-    [print(i) for i in data["Notes"]]
+    print('Current notes:\n')
+    [print(i) for i in data["Notes"]] if len(data["Notes"]) != 0 else print("You dont have any note yet.\n")
 
 data = get_data()
 print('TO-DO List ready for notes!\n')
 while True:
-    note = input("Type note or key word for exit: EXIT\n")
+    show_notes(data)
+    note = input("\nType note or key word for exit: EXIT\n")
     if note == 'EXIT':
         print('\nProgramm finished. All data saved.')
         break
     data["Notes"].append(note)
     in_data()
-    show_notes(data)
