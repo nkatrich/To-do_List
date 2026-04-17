1c2bda99536c6493d45b53e281e1ec3bda8f3b7c
Nikita <nikitakatrich.kzn@gmail.com>
2026-04-17 18:27:22 +0300

    removed unnecessary code, shorted code and name of vars

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import json
-import os
 
-file_name = "data.json"
+fl_nm = "data.json"
 
 def in_data():
-    with open(file_name, "w") as file:
-        json.dump(data, file, indent=4)
+    with open(fl_nm, "w") as f:
+        json.dump(data, f, indent=4)
         print("Data have been saved!\n")
 
 def get_data():
-    if os.path.exists(file_name):
-        with open(file_name, "r") as file:
-            return json.load(file)
+    with open(fl_nm, "r") as f:
+        return json.load(f)
 
 def show_notes(data):
     print('Current notes:\n')
     [print(f'\033[1m{i}\033[0m') for i in data["Notes"]] if len(data["Notes"]) != 0 else print("\033[1mYou dont have any note yet.\033[0m")
 
-data = get_data()
 print(f'\033[1m{25 * '*'} TO-DO List ready for notes! {25 * '*'}\033[0m\n')
+data = get_data()
+
 while True:
     show_notes(data)
     note = input("\n\u001b[32mType note or key word for exit: EXIT\033[0m\n")
